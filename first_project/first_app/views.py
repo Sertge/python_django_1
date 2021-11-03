d3d3ce3c1f0a3047aa275a337f27c5b76baeab7a
LAPTOP-5VLIPF13\Sertge <sergiojaime@ballastlane.com>
2021-11-02 23:49:47 -0500

    started model generation

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
- 
+    return HttpResponse("<h1>Hello World!</h1>")
+
+def bootstrap(request):
+    my_dict = {'insert_me':"Hello, views.py"}
+    return render(request,'bootstrap1.html',context=my_dict)
